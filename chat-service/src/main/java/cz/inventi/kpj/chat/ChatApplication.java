b82d6f9d24b934a6fe745a7b640f31a1e99cd9b6
Ondrej Turna <turna.o@seznam.cz>
2023-10-26 18:25:32 +0200

    added annotation

--- chat-service/src/main/java/cz/inventi/kpj/chat/ChatApplication.java
+++ chat-service/src/main/java/cz/inventi/kpj/chat/ChatApplication.java
@@ -8,6 +8,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 // TODO: enable JMS and scheduling
+@EnableJms
+@EnableScheduling
 public class ChatApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChatApplication.class, args);
